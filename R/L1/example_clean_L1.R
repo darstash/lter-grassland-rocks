8a63583e559e3bbe9eebc34ede2945909eed483c
moriahy <yomoriah@gmail.com>
2023-11-02 14:27:57 -0400

    making example scripts to make L1 and L2 folders

---
+++ R/L1/example_clean_L1.R
@@ -0,0 +1,18 @@
+# TITLE:          LTER Grassland Rock: KBS ANPP biomass and plant composition cleanup
+# AUTHORS:        
+# COLLABORATORS:  
+# DATA INPUT:     Data imported as csv files from shared Google drive L0 folder
+# DATA OUTPUT:    
+# PROJECT:        LTER Grassland Rock
+# DATE:           
+
+# Clear all existing data
+rm(list=ls())
+
+#Load packages
+library(tidyverse)
+
+# Set working directory 
+Sys.getenv("L0DIR")
+L0_dir <- Sys.getenv("L0DIR")
+L1_dir <- Sys.getenv("L1DIR")

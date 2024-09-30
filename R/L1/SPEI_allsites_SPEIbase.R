0345447cde4868b8a5e4b87edb3ebbc9072de0ad
darstash <darst017@umn.edu>
2024-09-30 14:04:57 -0400

    Add spei 3, 6, and 9

--- R/L1/SPEI_allsites_SPEIbase.R
+++ R/L1/SPEI_allsites_SPEIbase.R
@@ -1,5 +1,5 @@
 # TITLE:        LTER Grassland Rock: SPEI from SPEIbase for all sites
-# AUTHORS:      Caitlin Broderick
+# AUTHORS:      Caitlin Broderick, Ashley Darst
 # COLLABORATORS:  
 # DATA INPUT:   Data imported as csv files from shared Google drive L0 folder
 # DATA OUTPUT:  (not yet, but generate a list of years /spei/spei categories to match up with plant data)
@@ -30,10 +30,26 @@ L0_dir <- Sys.getenv("L0DIR")
 L1_dir <- Sys.getenv("L1DIR")
 list.files(L0_dir)
 
+# Read in datasets
+# SPEI-12
 kbs <- read.csv(file.path(L0_dir, "KBS/-85.250000_42.250000.csv"), stringsAsFactors = FALSE)
 knz <- read.csv(file.path(L0_dir, "KNZ/-96.750000_39.250000.csv"), stringsAsFactors = FALSE)
 cdr <- read.csv(file.path(L0_dir, "CDR/-93.250000_45.250000.csv"), stringsAsFactors = FALSE)
 
+#SPEI-9
+kbs9 <- read.csv(file.path(L0_dir, "KBS/-85.250000_42.250000_spei9.csv"), stringsAsFactors = FALSE)
+knz9 <- read.csv(file.path(L0_dir, "KNZ/-96.750000_39.250000_spei9.csv"), stringsAsFactors = FALSE)
+cdr9 <- read.csv(file.path(L0_dir, "CDR/-93.250000_45.250000_spei9.csv"), stringsAsFactors = FALSE)
+
+#SPEI-6
+kbs6 <- read.csv(file.path(L0_dir, "KBS/-85.250000_42.250000_spei6.csv"), stringsAsFactors = FALSE)
+knz6 <- read.csv(file.path(L0_dir, "KNZ/-96.750000_39.250000_spei6.csv"), stringsAsFactors = FALSE)
+cdr6 <- read.csv(file.path(L0_dir, "CDR/-93.250000_45.250000_spei6.csv"), stringsAsFactors = FALSE)
+
+#SPEI-3
+kbs3 <- read.csv(file.path(L0_dir, "KBS/-85.250000_42.250000_spei3.csv"), stringsAsFactors = FALSE)
+knz3 <- read.csv(file.path(L0_dir, "KNZ/-96.750000_39.250000_spei3.csv"), stringsAsFactors = FALSE)
+cdr3 <- read.csv(file.path(L0_dir, "CDR/-93.250000_45.250000_spei3.csv"), stringsAsFactors = FALSE)
 
 # separate columns
 # have to do it here! no maatter what I do, opening this in excel goofs it all up
@@ -41,16 +57,51 @@ cdr <- cdr %>%  separate_wider_delim(dates.spei12, delim = ";", names = c("date"
 kbs <- kbs %>%  separate_wider_delim(dates.spei12, delim = ";", names = c("date", "spei12"))
 knz <- knz %>%  separate_wider_delim(dates.spei12, delim = ";", names = c("date", "spei12"))
 
+cdr9 <- cdr9 %>%  separate_wider_delim(dates.spei09, delim = ";", names = c("date", "spei9"))
+kbs9 <- kbs9 %>%  separate_wider_delim(dates.spei09, delim = ";", names = c("date", "spei9"))
+knz9 <- knz9 %>%  separate_wider_delim(dates.spei09, delim = ";", names = c("date", "spei9"))
+
+cdr6 <- cdr6 %>%  separate_wider_delim(dates.spei06, delim = ";", names = c("date", "spei6"))
+kbs6 <- kbs6 %>%  separate_wider_delim(dates.spei06, delim = ";", names = c("date", "spei6"))
+knz6 <- knz6 %>%  separate_wider_delim(dates.spei06, delim = ";", names = c("date", "spei6"))
+
+cdr3 <- cdr3 %>%  separate_wider_delim(dates.spei03, delim = ";", names = c("date", "spei3"))
+kbs3 <- kbs3 %>%  separate_wider_delim(dates.spei03, delim = ";", names = c("date", "spei3"))
+knz3 <- knz3 %>%  separate_wider_delim(dates.spei03, delim = ";", names = c("date", "spei3"))
+
+# merge spei by date
+cdr <- merge(cdr, cdr3, by = "date")
+cdr <- merge(cdr, cdr6, by = "date")
+cdr <- merge(cdr, cdr9, by = "date")
+
+kbs <- merge(kbs, kbs3, by = "date")
+kbs <- merge(kbs, kbs6, by = "date")
+kbs <- merge(kbs, kbs9, by = "date")
+
+knz <- merge(knz, knz3, by = "date")
+knz <- merge(knz, knz6, by = "date")
+knz <- merge(knz, knz9, by = "date")
+
 # lubridate the date columns
 kbs$date <- lubridate::ymd(as.character(kbs$date))
 knz$date <- lubridate::ymd(as.character(knz$date))
 cdr$date <- lubridate::ymd(as.character(cdr$date))
 
-
 kbs$spei12 <- as.numeric (as.character(kbs$spei12))
 knz$spei12 <- as.numeric (as.character(knz$spei12))
 cdr$spei12 <- as.numeric (as.character(cdr$spei12))
 
+kbs$spei3 <- as.numeric (as.character(kbs$spei3))
+knz$spei3 <- as.numeric (as.character(knz$spei3))
+cdr$spei3 <- as.numeric (as.character(cdr$spei3))
+
+kbs$spei6 <- as.numeric (as.character(kbs$spei6))
+knz$spei6 <- as.numeric (as.character(knz$spei6))
+cdr$spei6 <- as.numeric (as.character(cdr$spei6))
+
+kbs$spei9 <- as.numeric (as.character(kbs$spei9))
+knz$spei9 <- as.numeric (as.character(knz$spei9))
+cdr$spei9 <- as.numeric (as.character(cdr$spei9))
 
 # add year column for graphing purposes
 kbs$year <- year(kbs$date)
@@ -79,6 +130,7 @@ mean(cdr.aug$spei12)  # looking good.
 # calculate interquartile range - "normal range"
 # this is the diffeerence between upper and lower quartile ranges. 
 # Calculate 1 in 10 year event 
+# the isbell paper does this a little differently FYI
 
 quantile(kbs.aug$spei12, c(0.1, 0.25, 0.75, 0.9))
 quantile(knz.aug$spei12, c(0.1, 0.25, 0.75, 0.9))
@@ -87,7 +139,7 @@ quantile(cdr.aug$spei12, c(0.1, 0.25, 0.75, 0.9))
 
 # add column for classifying years based on quartiles !!!!!
 
-kbs.aug <-kbs.aug   %>% mutate(spei_category = case_when(
+kbs.aug <-kbs.aug   %>% mutate(spei12_category = case_when(
   kbs.aug$spei12 < -1.4 ~ "Extreme dry", 
   kbs.aug$spei12 >= -1.4 & kbs.aug$spei12 < -0.7 ~ "Moderate dry", 
   kbs.aug$spei12 >= -0.7 & kbs.aug$spei12 <= 0.8 ~ "Normal", 
@@ -97,7 +149,7 @@ kbs.aug <-kbs.aug   %>% mutate(spei_category = case_when(
 
 
 # add column for classifying years based on quartiles !!!!!
-knz.aug <-knz.aug   %>% mutate(spei_category = case_when(
+knz.aug <-knz.aug   %>% mutate(spei12_category = case_when(
   knz.aug$spei12 < -1.4 ~ "Extreme dry", 
   knz.aug$spei12 >= -1.4 & knz.aug$spei12 < -0.7 ~ "Moderate dry", 
   knz.aug$spei12 >= -0.7 & knz.aug$spei12 <= 0.7 ~ "Normal", 
@@ -107,7 +159,7 @@ knz.aug <-knz.aug   %>% mutate(spei_category = case_when(
 
 
 # add column for classifying years based on quartiles !!!!!
-cdr.aug <-cdr.aug   %>% mutate(spei_category = case_when(
+cdr.aug <-cdr.aug   %>% mutate(spei12_category = case_when(
   cdr.aug$spei12 < -1.3 ~ "Extreme dry", 
   cdr.aug$spei12 >= -1.3 & cdr.aug$spei12 < -0.8 ~ "Moderate dry", 
   cdr.aug$spei12 >= -0.8 & cdr.aug$spei12 <= 0.7 ~ "Normal", 
@@ -117,13 +169,13 @@ cdr.aug <-cdr.aug   %>% mutate(spei_category = case_when(
 
 # order factor
 
-kbs.aug$spei_category <- factor(kbs.aug$spei_category, levels = c("Extreme dry", "Moderate dry", "Normal", "Moderate wet", "Extreme wet"))
-knz.aug$spei_category <- factor(knz.aug$spei_category, levels = c("Extreme dry", "Moderate dry", "Normal", "Moderate wet", "Extreme wet"))
-cdr.aug$spei_category <- factor(cdr.aug$spei_category, levels = c("Extreme dry", "Moderate dry", "Normal", "Moderate wet", "Extreme wet"))
+kbs.aug$spei12_category <- factor(kbs.aug$spei12_category, levels = c("Extreme dry", "Moderate dry", "Normal", "Moderate wet", "Extreme wet"))
+knz.aug$spei12_category <- factor(knz.aug$spei12_category, levels = c("Extreme dry", "Moderate dry", "Normal", "Moderate wet", "Extreme wet"))
+cdr.aug$spei12_category <- factor(cdr.aug$spei12_category, levels = c("Extreme dry", "Moderate dry", "Normal", "Moderate wet", "Extreme wet"))
 
 kbs_spei <- ggplot(kbs.aug , aes (x = year, y = spei12)) +
   geom_line(alpha = 0.5) + 
-  geom_point( aes(color = spei_category), size = 1.25) + theme_bw() +
+  geom_point( aes(color = spei12_category), size = 1.25) + theme_bw() +
   scale_x_continuous(breaks = seq(1900, 2025, by = 10)) +
   annotate( "text", label = "KBS",
     x = 1910, y = 2, size = 5, colour = "black") + 
@@ -134,7 +186,7 @@ ggsave("KBS_SPEI.png",plot =kbs_spei, dpi = 300, width =8, height = 4, units = "
 
 knz_spei <- ggplot(knz.aug , aes (x = year, y = spei12)) +
   geom_line(alpha = 0.5) + 
-  geom_point( aes(color = spei_category), size = 1.25) + theme_bw() +
+  geom_point( aes(color = spei12_category), size = 1.25) + theme_bw() +
   scale_x_continuous(breaks = seq(1900, 2025, by = 10)) +
   annotate( "text", label = "KNZ",
             x = 1910, y = 2, size = 5, colour = "black") + 
@@ -145,7 +197,7 @@ ggsave("KNZ_SPEI.png",plot =knz_spei, dpi = 300, width =8, height = 4, units = "
 
 cdr_spei <- ggplot(cdr.aug , aes (x = year, y = spei12)) +
   geom_line(alpha = 0.5) + 
-  geom_point( aes(color = spei_category), size = 1.25) + theme_bw() +
+  geom_point( aes(color = spei12_category), size = 1.25) + theme_bw() +
   scale_x_continuous(breaks = seq(1900, 2025, by = 10)) +
   annotate( "text", label = "CDR",
             x = 1910, y = 2, size = 5, colour = "black") + 

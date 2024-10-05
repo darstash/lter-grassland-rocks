c5329777effbe69d2840b99b017ae97e77dcc4de
manieland <mnieland@umass.edu>
2024-10-05 13:53:13 -0500

    This script calculate Berger-Parker dominance index and species richness.

---
+++ R/L2/DominanceRichnessCalculation.R
@@ -0,0 +1,17 @@
+library(tidyverse)
+
+L2_dir <- Sys.getenv("L2DIR")
+
+list.files(L2_dir)
+
+species_abundance_SPEI <- read.csv(file.path(L2_dir, "species_abundance_SPEI.csv"), stringsAsFactors = F)
+
+species_abundance_SPEI_Metric <- species_abundance_SPEI %>% 
+  group_by(year, site, dataset, plot, higher_order_organization, uniqueid,
+           cover_method, area_sampled_bio, area_sampled_cover,
+           spei12, spei3, spei6, spei9, spei12_category) %>% 
+  summarize(Berger_Parker = max(relative_abundance),
+            Richness = n()) %>% 
+  select(year, site, uniqueid, Berger_Parker, Richness)
+
+write.csv(species_abundance_SPEI_Metric, file.path(L2_dir, "./plot_metrics_SPEI_diversity.csv"), row.names=F)

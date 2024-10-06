d623b9f8b975aba87353dedd4223e8acee88ab6b
manieland <mnieland@umass.edu>
2024-10-06 12:20:27 -0500

    Corrected richness calculation added plot.

--- R/L2/DominanceRichnessCalculation.R
+++ R/L2/DominanceRichnessCalculation.R
@@ -8,15 +8,21 @@ species_abundance_SPEI <- read.csv(file.path(L2_dir, "species_abundance_SPEI.csv
 plot_metrics_SPEI <- read.csv(file.path(L2_dir, "plot_metrics_SPEI.csv"), stringsAsFactors = F)
 
 species_abundance_SPEI_Metric <- species_abundance_SPEI %>% 
+  filter(relative_abundance > 0) %>% 
   group_by(year, site, dataset, plot, higher_order_organization, uniqueid,
            cover_method, area_sampled_bio, area_sampled_cover,
            spei12, spei3, spei6, spei9, spei12_category) %>% 
   summarize(Berger_Parker = max(relative_abundance),
             Richness = n())
 
+ggplot(species_abundance_SPEI_Metric, aes(x = Berger_Parker, y = Richness))+
+  geom_point(color = "black", alpha = 1/3)+
+  theme_bw()
+
 plot_metrics_SPEI_diversity <- plot_metrics_SPEI %>% 
-  left_join(., species_abundance_SPEI_Metric, by = c("year", "site",  "higher_order_organization", "plot", "uniqueid",
+  right_join(., species_abundance_SPEI_Metric, by = c("year", "site",  "higher_order_organization", "plot", "uniqueid",
                                                      "spei12", "spei3", "spei6", "spei9", "spei12_category"))
 
 
 write.csv(plot_metrics_SPEI_diversity, file.path(L2_dir, "./plot_metrics_SPEI_diversity.csv"), row.names=F)
+

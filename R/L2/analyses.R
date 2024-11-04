81e3dda06b2de8c40220cb90a575bcbd63715e84
darstash <darst017@umn.edu>
2024-11-04 16:20:32 -0500

    Compraed more models for resistance and added year to random effects structure

--- R/L2/analyses.R
+++ R/L2/analyses.R
@@ -51,7 +51,7 @@ plot_ece <- clean_names(plot_ece)
 # Analysis 1: resistance vs richness ----
 
 # Model 1
-resist.lm <- lmer(resistance ~ richness + (1|site) + (1|site:plot), data = plot_ece)
+resist.lm <- lmer(resistance ~ richness + (1|site) + (1|site:plot) + (1|year), data = plot_ece)
 summary(resist.lm) # boundary singular fit, site explains 0 variance
 
 # Check residuals and QQplot
@@ -59,7 +59,7 @@ simres <- simulateResiduals(resist.lm)
 plot(simres) # Very bad!
 
 # Model 2 - try logging resistance
-resist.lm.log <- lmer(log10(resistance) ~ richness + (1|site) + (1|site:plot), data = plot_ece)
+resist.lm.log <- lmer(log10(resistance) ~ richness + (1|site) + (1|site:plot) + (1|year), data = plot_ece)
 summary(resist.lm.log)
 
 # Check residuals and QQplot
@@ -67,7 +67,7 @@ simres <- simulateResiduals(resist.lm.log)
 plot(simres) # Better, but not perfect
 
 # Model 3 - try logging richness
-resist.lm.log2 <- lmer(log10(resistance) ~ log10(richness) + (1|site) + (1|site:plot), data = plot_ece)
+resist.lm.log2 <- lmer(log10(resistance) ~ log10(richness) + (1|site) + (1|site:plot) + (1|year), data = plot_ece)
 summary(resist.lm.log2)
 
 # Check residuals and QQplot
@@ -75,47 +75,70 @@ simres <- simulateResiduals(resist.lm.log2)
 plot(simres) # Didn't help
 
 # Model 4 - add wet vs dry event to model 2
-resist.lm.wd <- lmer(log10(resistance) ~ richness + spei6_category + (1|site) + (1|site:plot), data = plot_ece)
+resist.lm.wd <- lmer(log10(resistance) ~ richness + spei6_category + (1|site) + (1|site:plot) + (1|year), data = plot_ece)
 summary(resist.lm.wd)
+resist.lm.wd.log <- lmer(log10(resistance) ~ log10(richness) + spei6_category + (1|site) + (1|site:plot) + (1|year), data = plot_ece)
 
 # Check residuals and QQplot
 simres <- simulateResiduals(resist.lm.wd)
 plot(simres) # Didn't help
 
 # Model 5 - try interaction with category
-resist.lm.wd.int <- lmer(log10(resistance) ~ richness*spei6_category + (1|site) + (1|site:plot), data = plot_ece)
+resist.lm.wd.int <- lmer(log10(resistance) ~ richness*spei6_category + (1|site) + (1|site:plot) + (1|year), data = plot_ece)
 summary(resist.lm.wd.int)
+resist.lm.wd.int.log <- lmer(log10(resistance) ~ log10(richness)*spei6_category + (1|site) + (1|site:plot) + (1|year), data = plot_ece)
 
 # Check residuals and QQplot
 simres <- simulateResiduals(resist.lm.wd.int)
 plot(simres) # Didn't help
 
 # Model 6 - add dominance
-resist.lm.dom <- lmer(log10(resistance) ~ richness*spei6_category + berger_parker + (1|site) + (1|site:plot), data = plot_ece)
+resist.lm.dom <- lmer(log10(resistance) ~ richness*spei6_category + berger_parker + (1|site) + (1|site:plot) + (1|year), data = plot_ece)
 summary(resist.lm.dom)
+resist.lm.dom.log <- lmer(log10(resistance) ~ log10(richness)*spei6_category + berger_parker + (1|site) + (1|site:plot) + (1|year), data = plot_ece)
 
 # Check residuals and QQplot
 simres <- simulateResiduals(resist.lm.dom)
 plot(simres) # Didn't help
 
 # Model 7 - add dominance interaction with richness
-resist.lm.dom.int <- lmer(log10(resistance) ~ richness*spei6_category + berger_parker*richness + (1|site) + (1|site:plot), data = plot_ece)
+resist.lm.dom.int <- lmer(log10(resistance) ~ richness*spei6_category + berger_parker*richness + (1|site) + (1|site:plot) + (1|year), data = plot_ece)
 summary(resist.lm.dom.int)
+resist.lm.dom.int.log <- lmer(log10(resistance) ~ log10(richness)*spei6_category + berger_parker*log10(richness) + (1|site) + (1|site:plot) + (1|year), data = plot_ece)
 
 # Check residuals and QQplot
 simres <- simulateResiduals(resist.lm.dom.int)
 plot(simres) # Didn't help
 
-# Compare models
-AICctab(resist.lm.log, resist.lm.wd, resist.lm.wd.int, resist.lm.dom, resist.lm.dom.int)
+# Compare models # Depends on how/if you log richness
+AICctab(resist.lm.log, resist.lm.log2, resist.lm.wd, resist.lm.wd.log, resist.lm.wd.int, resist.lm.wd.int.log, resist.lm.dom, resist.lm.dom.log, resist.lm.dom.int, resist.lm.dom.int.log)
 
 # Plot best model
-ggpredict(model = resist.lm.wd, terms = c("richness", "spei6_category"), back_transform = F) %>%
+ggpredict(model = resist.lm.log2, terms = c("richness"), back_transform = F) %>%
   plot(show_data = TRUE)
 
 # Random effects like Isbell # Not sure if this is right
-resist.lm.r <- lmer(log10(resistance) ~ richness*spei6_category + (1|site/(richness:year)) + (1|plot), data = plot_ece, na.action=na.omit)
+resist.lm.r <- lmer(log10(resistance) ~ richness*spei6_category + (1|site:richness:year) + (1|site:richness) + (1|site:year) + (1|site:plot) + (1|site), data = plot_ece, na.action=na.omit)
 summary(resist.lm.r)
+
+resist.lm.r2 <- lmer(log10(resistance) ~ richness + spei6_category + (1|site:richness:year) + (1|site:richness) + (1|site:year) + (1|site:plot) + (1|site), data = plot_ece, na.action=na.omit)
+resist.lm.r2.log <- lmer(log10(resistance) ~ log10(richness) + spei6_category + (1|site:richness:year) + (1|site:richness) + (1|site:year) + (1|site:plot) + (1|site), data = plot_ece, na.action=na.omit)
+resist.lm.r2.log2 <- lmer(log2(resistance) ~ log2(richness) + spei6_category + (1|site:richness:year) + (1|site:richness) + (1|site:year) + (1|site:plot) + (1|site), data = plot_ece, na.action=na.omit)
+resist.lm.r3 <- lmer(log10(resistance) ~ richness + spei6_category + berger_parker + (1|site:richness:year) + (1|site:richness) + (1|site:year) + (1|site:plot) + (1|site), data = plot_ece, na.action=na.omit)
+resist.lm.r4 <- lmer(log10(resistance) ~ richness*spei6_category + berger_parker + (1|site:richness:year) + (1|site:richness) + (1|site:year) + (1|site:plot) + (1|site), data = plot_ece, na.action=na.omit)
+resist.lm.r5 <- lmer(log10(resistance) ~ richness*spei6_category + berger_parker*spei6_category + (1|site:richness:year) + (1|site:richness) + (1|site:year) + (1|site:plot) + (1|site), data = plot_ece, na.action=na.omit)
+
+# Compare models with Isbell random effects
+AICctab(resist.lm.r, resist.lm.r2, resist.lm.r2.log, resist.lm.r2.log2, resist.lm.r3, resist.lm.r4, resist.lm.r5)
+
+# Look at model stats
+summary(resist.lm.r2.log)
+simres <- simulateResiduals(resist.lm.r2.log)
+plot(simres) # Not great
+
+# Plot best model
+ggpredict(model = resist.lm.r2.log, terms = c("richness", "spei6_category"), back_transform = F) %>%
+  plot(show_data = TRUE)
 
 
 ## Analysis 2 - resilience vs richness

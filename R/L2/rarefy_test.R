c626af5057fb9404ca15efcf9cd8a62db61e3e8e
darstash <darst017@umn.edu>
2025-01-14 12:53:49 -0500

    Can't rarefy in vegan, need to try something else

--- R/L2/rarefy_test.R
+++ R/L2/rarefy_test.R
@@ -24,15 +24,26 @@ plot <- read.csv(file.path(L2_dir, "plot_metrics.csv"))
 
 specabun_meta <- left_join(specabun, plot)
 
-specabun_wide <- specabun_meta %>%
+specabun_wide_filter <- specabun_meta %>%
   rowid_to_column() %>% # add due to duplicates
+  mutate(relative_abundance = as.integer(relative_abundance*10^6)) %>%
+  filter(rowid < 201 | rowid > 272800) %>%
   pivot_wider(names_from = species,
               names_sep = "_",
               values_from = relative_abundance,
-              values_fill = list(Value = 0))
-specabun_wide <- specabun_wide %>% mutate_all( ~replace(., lengths(.)==0, 0))
-specabun_wide[19:1475] <- sapply(specabun_wide[19:1475],as.numeric)
+              values_fill = 0)
 
-plot_area_vector <- specabun_wide$measurement_scale_cover
 
-rarefied_data <- rarefy(specabun_wide[19:1475], sample = plot_area_vector)
+plot_area_vector <- specabun_wide_filter$measurement_scale_cover
+plot_area_vector <- replace_na(plot_area_vector, 1)
+plot_area_vector <- plot_area_vector*10
+
+rarefied_data <- rarefy(specabun_wide_filter[20:144], sample = plot_area_vector)
+
+data(BCI)
+S <- specnumber(BCI) # observed number of species
+(raremax <- min(rowSums(BCI)))
+Srare <- rarefy(BCI, raremax)
+plot(S, Srare, xlab = "Observed No. of Species", ylab = "Rarefied No. of Species")
+abline(0, 1)
+rarecurve(BCI, step = 20, sample = raremax, col = "blue", cex = 0.6)

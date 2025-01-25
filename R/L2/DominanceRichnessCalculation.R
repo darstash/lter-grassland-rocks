be3620681dffbaf9c6b704ef8c1655fa2d352ad6
joshuaajowele@gmail.com <joshuaajowele@gmail.com>
2025-01-25 10:14:23 -0500

    Calculated evenness using the Evar metric in codyn package

--- R/L2/DominanceRichnessCalculation.R
+++ R/L2/DominanceRichnessCalculation.R
@@ -9,7 +9,11 @@
 # Clear all existing data
 rm(list=ls())
 
+#a community metric package that provides options for various evenness metrics.
+install.packages("codyn")
+library(codyn)
 library(tidyverse)
+
 
 L2_dir <- Sys.getenv("L2DIR")
 
@@ -30,9 +34,22 @@ ggplot(species_abundance_SPEI_Metric, aes(x = Berger_Parker, y = Richness))+
   geom_point(color = "black", alpha = 1/3)+
   theme_bw()
 
+#calculate evenness using EVar; Evar is independent of species richness and recommended by Smith and Wilson (1996)
+species_abundance_SPEI_evar<-species_abundance_SPEI%>%
+  filter(relative_abundance > 0)%>%
+  select(year, uniqueid, species, relative_abundance)%>%
+  distinct()
+#calculates evenness and richness
+Evenness_richness<-community_structure(species_abundance_SPEI_evar, time.var="year",
+                                       replicate.var = "uniqueid",
+                                       abundance.var="relative_abundance",metric="Evar")%>%
+  select(-richness)#remove richness since it was already calculated
+#monoculture produces NAs for evenness
+
 plot_metrics_SPEI_diversity <- plot_metrics_SPEI %>% 
   right_join(., species_abundance_SPEI_Metric, by = c("year", "site",  "higher_order_organization", "plot", "uniqueid",
-                                                     "spei12", "spei3", "spei6", "spei9", "spei6_category", "spei12_category"))
+                                                     "spei12", "spei3", "spei6", "spei9", "spei6_category", "spei12_category"))%>%
+  left_join(Evenness_richness, by = c("year","uniqueid"))
 
 
 write.csv(plot_metrics_SPEI_diversity, file.path(L2_dir, "./plot_metrics_SPEI_diversity.csv"), row.names=F)

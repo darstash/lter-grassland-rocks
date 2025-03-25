30c5c2c96583634673021ceeab8a4396b239331d
darstash <darst017@umn.edu>
2025-03-25 15:28:09 -0400

    Added loop for spei9

--- R/L2/all_sites_resistance_resilience.R
+++ R/L2/all_sites_resistance_resilience.R
@@ -21,6 +21,7 @@ list.files(L2_dir)
 # Read in CSV files
 plot_metrics <- read.csv(file.path(L2_dir, "plot_metrics_SPEI_diversity.csv"), stringsAsFactors = FALSE)
 
+# SPEI6 ----
 # Create dataframe that contains extreme years at each site
 extreme_per_site <- plot_metrics %>% 
   filter(spei6_category == "Extreme wet" | spei6_category == "Extreme dry")
@@ -55,6 +56,44 @@ for(i in 1:nrow(extreme_per_site)) {
 
 # save dataframe of plot resistance and resilience to ECEs as .csv in Google Drive L2 folder
 write.csv(df, file.path(L2_dir, "./ece_resist_resil.csv"), row.names=F)
+
+
+# SPEI9 ----
+# Create dataframe that contains extreme years at each site
+extreme_per_site9 <- plot_metrics %>% 
+  filter(spei9_category == "Extreme wet" | spei9_category == "Extreme dry")
+
+
+# Create dataframe that contains average "normal" biomass for each plot
+biomass_normal9 <- plot_metrics %>% 
+  filter(spei9_category == "Normal" | spei9_category == "Moderate wet" | spei9_category == "Moderate dry") %>% # not enough just normal years
+  group_by(uniqueid) %>% # group by plot
+  summarize(average_normal_biomass = mean(plot_biomass, na.rm=T)) #na.rm=T is required to avoid NAs when the plots are missing values for certain years
+
+# Create dataframe of metrics for plots during extreme climate events that includes resistance and resilience
+df9 <- data.frame(ex_year = integer(), site = factor(), uniqueid = character(), resistance = numeric(), resilience = numeric())
+
+for(i in 1:nrow(extreme_per_site9)) {
+  subs1 <- plot_metrics %>% 
+    filter(year %in% extreme_per_site9[i, "year"] &
+             site %in% extreme_per_site9[i,"site"]) %>% 
+    rename("biomass_during" = "plot_biomass") %>% 
+    rename("ex_year" = "year")
+  subs2 <- plot_metrics %>% 
+    filter(year %in% (extreme_per_site9[i, "year"] + 1) &
+             site %in% extreme_per_site9[i,"site"]) %>% 
+    rename("biomass_after" = "plot_biomass")
+  subs <- merge (subs1, subset(subs2, select = -c(year)), by = c("site", "uniqueid"))
+  subs <- merge(subs, biomass_normal9, by = c("uniqueid"), all.x = T)
+  subs$resistance <- subs$average_normal_biomass / abs((subs$biomass_during - subs$average_normal_biomass)) # resistance following Isbell et al. 2015
+  subs$resilience <- abs((subs$biomass_during - subs$average_normal_biomass) / (subs$biomass_after - subs$average_normal_biomass)) # resilience following Isbell et al. 2015
+  common_names <- intersect(colnames(df9), colnames(subs))
+  df9 <- rbind.data.frame(df9[, common_names], subs[, common_names])
+}
+
+# save dataframe of plot resistance and resilience to ECEs as .csv in Google Drive L2 folder
+write.csv(df9, file.path(L2_dir, "./ece_resist_resil_spei9.csv"), row.names=F)
+
 
 ### troubleshooting loop and calculations
 ##### loop for KBS data only for troubleshooting
